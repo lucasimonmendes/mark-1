a187e4bb48e0e5c62c51ba5b2528ace7dfd3ad9f
Lucas Simon <lucasimonmendes@hotmail.com>
2024-08-20 18:17:38 -0300

    :bug: fix i3 config file error

--- theme.sh
+++ theme.sh
@@ -4,6 +4,8 @@ FONT_DIR="$HOME/.local/share/fonts"
 FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Meslo.zip"
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 WALLPAPER_PATH="$SCRIPT_DIR/wallpaper.png"
+I3_CONFIG_DIR="$HOME/.config/i3"
+I3_CONFIG_FILE="$I3_CONFIG_DIR/config"
 
 # Função para verificar se o i3 está instalado
 check_package() {
@@ -37,13 +39,25 @@ else
   echo "Fonte NerdFont MesloGS instalada com sucesso."
 fi
 
+# Cria o diretório de configuração do i3 se não existir
+if [ ! -d "$I3_CONFIG_DIR" ]; then
+  echo "Criando o diretório de configuração do i3..."
+  mkdir -p "$I3_CONFIG_DIR"
+fi
+
+# Cria o arquivo de configuração do i3 se não existir
+if [ ! -f "$I3_CONFIG_FILE" ]; then
+  echo "Criando o arquivo de configuração do i3..."
+  touch "$I3_CONFIG_FILE"
+fi
+
 # Configura o papel de parede no i3
 if [ -f "$WALLPAPER_PATH" ]; then
-  if grep -q "feh --bg-scale $WALLPAPER_PATH" ~/.config/i3/config; then
+  if grep -q "feh --bg-scale $WALLPAPER_PATH" "$I3_CONFIG_FILE"; then
     echo "O papel de parede já está configurado no i3."
   else
     echo "Configurando o papel de parede no i3..."
-    echo "exec --no-startup-id feh --bg-scale $WALLPAPER_PATH" >>~/.config/i3/config
+    echo "exec --no-startup-id feh --bg-scale $WALLPAPER_PATH" >>"$I3_CONFIG_FILE"
     echo "Papel de parede configurado com sucesso."
   fi
 else
