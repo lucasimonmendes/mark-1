3e94f9dbf109f56e91d345d6372bea66952c04a9
Lucas Simon <lucasimonmendes@hotmail.com>
2024-08-28 17:30:53 -0300

    :sparkles: update program with dialog and formating

--- mark-1.sh
+++ mark-1.sh
@@ -1,38 +1,147 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Arte ASCII
-echo "___  ___           _               __  "
-echo "|  \/  |          | |             /  | "
-echo "| .  . | __ _ _ __| | __  ______ \`| | "
-echo "| |\/| |/ _\` |'__| |/ / |______|  | | "
-echo "| |  | | (_| | |  |   <           _| |_"
-echo "\_|  |_/\__,_|_|  |_|\_\          \___/"
-echo "                                       "
-echo "                                       "
+###############################################################################
+# Author: Lucas Simon <lucasimonmendes@gmail.com>
+#
+# Program name: Mark 1
+# Version: 0.2
+# License: MIT
+# Description: This program installs the necessary tools to work with the terminal and styling and window manager for productivity.
+#
+# CHANGELOG:
+# 21/08/2024 - Lucas Simon
+#   - [Added] styling features and program downloads.
+# 28/08/2024 - Lucas Simon
+#   - [Added] formating program and add the dialog interface.
+###############################################################################
+set -e # if there is an error, exit
 
-echo "Iniciando a configuração do ambiente..."
+############################################ Variables
+readonly programName="Mark-1"
+readonly programVersion="0.2"
 
-# Executa o script de configuração do terminal
-./terminal.sh
+######################################################
 
-# Executa o script de configuração do tema
-./theme.sh
-
-# Checagem final da instalação
-echo "Verificando se todos os pacotes foram instalados corretamente..."
-
-MISSING_PACKAGES=()
-PACKAGES=("neovim" "tmux" "tmuxinator" "curl" "unzip" "i3" "feh" "xbacklight" "xinput" "i3blocks" "rxvt-unicode-256color" "screen" "scrot" "redshift" "alsa-utils" "acpi" "fonts-font-awesome" "xdotool" "xclip")
-for package in "${PACKAGES[@]}"; do
-  if ! dpkg -l | grep -q "$package"; then
-    MISSING_PACKAGES+=("$package")
-  fi
+############################################ Tests
+for check in 'dialog' 'curl'; do
+	if ! which "$check" &>/dev/null; then
+		echo "Necessario ter $check instalado."
+		exit 1
+	fi
 done
 
-if [ ${#MISSING_PACKAGES[@]} -eq 0 ]; then
-  echo "Todos os pacotes foram instalados com sucesso."
-else
-  echo "Os seguintes pacotes não foram instalados corretamente: ${MISSING_PACKAGES[*]}"
-fi
+check_package() {
+	dpkg -l | grep -q "$1"
+}
 
-echo "Configuração do ambiente concluída."
+##################################################
+
+############################################ Functions
+
+function checkInstalation() {
+	dialog --title "Checando..." --sleep 2 --infobox "Checando se os pacotes foram instalados corretamente, aguarde!" 5 60
+	MISSING_PACKAGES=()
+	for package in "${PACKAGES[@]}"; do
+		if ! dpkg -l | grep -q "$package"; then
+			MISSING_PACKAGES+=("$package")
+		fi
+	done
+
+	if [ ${#MISSING_PACKAGES[@]} -eq 0 ]; then
+		dialog --title "Pacotes instalados ;)" --msgbox "Todos os pacotes foram instalados com sucesso." 5 60
+	else
+		dialog --title "Erro :(" --msgbox "Os seguintes pacotes não foram instalados corretamente: ${MISSING_PACKAGES[*]}" 6 50
+	fi
+}
+
+function installTerminal() {
+	#	updatePackages
+
+	dialog --title "Instalando ferramentas..." --sleep 2 --infobox "Instalando ferramentas do terminal." 5 60
+	local terminalApps=("tmux" "tmuxinator" "curl" "unzip" "git")
+
+	for package in "${terminalApps[@]}"; do
+		if check_package "$package"; then
+			dialog --title "$package instalado!" --sleep 3 --infobox "$package já está instalado." 5 60
+		else
+			dialog --title "Instalando $package" --sleep 3 --infobox "Instalando $package via apt..." 5 60
+			sudo apt install -y "$package"
+		fi
+	done
+
+	checkInstalation terminalApps
+}
+
+function installStyles() {
+	dialog --title "Instalando estilos..." --sleep 2 --infobox "Instalando estilos e fontes." 5 60
+	local styleApps=("i3" "feh" "xbacklight" "xinput" "i3blocks" "screen" "scrot" "redshift" "alsa-utils" "acpi" "fonts-font-awesome" "xdotool" "xclip")
+
+	for package in "${styleApps[@]}"; do
+		if check_package "$package"; then
+			dialog --title "$package instalado!" --sleep 3 --infobox "$package já está instalado." 5 60
+		else
+			dialog --title "Instalando $package" --sleep 3 --infobox "Instalando $package via apt..." 5 60
+			sudo apt install -y "$package"
+		fi
+	done
+
+	checkInstalation styleApps
+
+	installFonts
+
+}
+
+function installFonts() {
+	local FONT_DIR="$HOME/.local/share/fonts"
+	local FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Meslo.zip"
+
+	if [ -d "$FONT_DIR/Meslo" ]; then
+		dialog --title "Fonte ja instalada" --sleep 2 --infobox "A fonte NerdFont MesloGS já está instalada." 5 60
+	else
+		dialog --title "Baixando fonte..." --sleep 2 --infobox "Baixando e instalando a fonte NerdFont MesloGS..." 5 60
+		mkdir -p "$FONT_DIR"
+		cd "$FONT_DIR"
+		curl -fLo "Meslo.zip" "$FONT_URL"
+		unzip Meslo.zip -d Meslo
+		rm Meslo.zip
+		fc-cache -fv
+		dialog --title "Fonte instalada!" --msgbox "Fonte NerdFont MesloGS instalada com sucesso." 5 60
+	fi
+
+}
+# function updatePackages() {
+# 	dialog --title "Atualizando pacotes" --sleep 2 --infobox "Atualizando a lista de pacotes..." 5 60
+# 	sudo apt update &>/dev/null
+# }
+######################################################
+
+############################################ Main
+
+# Menu
+menu=$(
+	dialog --stdout \
+		--backtitle "$programName - $programVersion" \
+		--title "$programName" \
+		--item-help \
+		--menu "O que voce deseja fazer?" \
+		0 0 0 \
+		1 "Instalar ferramentas do terminal." \
+		"Instala as ferramentas essenciais para programar com editor de texto, multiplexador de terminal e git." \
+		2 "Instalar ferramentas e gerenciador de janelas." \
+		"Instala fontes e ferramentas como window manager para a produtividade e estilo."
+)
+
+# O que o usuario escolheu
+case $menu in
+1) installTerminal ;;
+2) installStyles ;;
+esac
+
+# echo "Iniciando a configuração do ambiente..." # vai virar um dialog
+#
+# # Checagem final da instalação -> Checar logo apos terminar cada funcao
+# echo "Verificando se todos os pacotes foram instalados corretamente..."
+#
+#
+#
+# echo "Configuração do ambiente concluída."
